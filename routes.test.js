843e21aea2bfcb293b810fdb71432ba946a47fa4
SunJieMing <bennelson@bens-mbp.attlocal.net>
2017-09-07 14:30:17 -0700

    Added solution

--- routes.test.js
+++ routes.test.js
@@ -11,7 +11,22 @@ const chaiHTTP = require('chai-http');
 chai.use(chaiHTTP);
 
 describe('/food', () => {
+  let foodId;
+
   beforeEach((done) => {
+    new Food({
+      name: 'Rib Steak'
+    }).save((err, savedFood) => {
+      if (err) {
+        console.log(err);
+        return done();
+      }
+      foodId = savedFood.id;
+      done();
+    });
+  });
+
+  afterEach((done) => {
     Food.remove({}, (err) => {
       if (err) console.log(err);
       done();
@@ -26,8 +41,47 @@ describe('/food', () => {
           if (err) return console.log(err);
           expect(res.status).to.equal(200);
           expect(Array.isArray(res.body)).to.equal(true);
-          expect(res.body.length).to.equal(0);
+          expect(res.body.length).to.equal(1);
           done();
+        });
+    });
+  });
+
+  describe('[PUT] /food', () => {
+    it('should update the food document', (done) => {
+      const update = {
+        id: foodId,
+        name: 'Tomahawk Steak'
+      };
+      chai.request(server)
+        .put('/food')
+        .send(update)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.body.name).to.equal('Tomahawk Steak');
+          done();
+        });
+    });
+  });
+
+  describe('[DELETE] /food/:id', () => {
+    it('should remove the specified document', (done) => {
+      chai.request(server)
+        .delete(`/food/${foodId}`)
+        .end((err, res) => {
+          if (err) {
+            console.log(err);
+            return done();
+          }
+          expect(res.text).to.equal('success');
+          Food.findById(foodId, (err, deletedFood) => {
+            if (err) {
+              console.log(err);
+              return done();
+            }
+            expect(deletedFood).to.equal(null);
+            done();
+          });
         });
     });
   });
--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Food = require('./food');
+const morgan = require('morgan');
 
 const server = express();
 server.use(bodyParser.json());
+server.use(morgan('combined'));
 
 server.get('/food', (req, res) => {
   Food.find({}, (err, food) => {
     if (err) return res.send(err);
     res.send(food);
+  });
+});
+
+server.put('/food', (req, res) => {
+  Food.findById(req.body.id, (err, food) => {
+    food.name = req.body.name;
+    food.save((err, updatedFood) => {
+      if (err) return res.send(err);
+      res.send(updatedFood);
+    });
+  });
+});
+
+server.delete('/food/:id', (req, res) => {
+  Food.findById(req.params.id).remove((err, removedFood) => {
+    if (err) return res.send(err);
+    res.send(`success`);
   });
 });
 
